e6020519924d754fd5cee19193d04ceb2dddc4e1
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2020-05-15 11:27:27 +0200

    Her er nogle klasser til bruger og samkørsler

---
+++ ZeaRide/Model/Bruger.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeaRide.Model
+{
+    class Bruger
+    {
+        private int _brugerId;
+        private string _fornavn;
+        private string _efternavn;
+        private string _email;
+        private string _password;
+        private string _by;
+        private int _postnummer;
+        private string _bilmodel;
+        private string _bilfarve;
+
+        public Bruger(int BrugerId, string Fornavn, string Efternavn, string Email, string Password, string By, int Postnummer, string Bilmodel, string Bilfarve)
+        {
+            _brugerId = BrugerId;
+            _fornavn = Fornavn;
+            _efternavn = Efternavn;
+            _email = Email;
+            _password = Password;
+            _by = By;
+            _postnummer = Postnummer;
+            _bilmodel = Bilmodel;
+            _bilfarve = Bilfarve;
+        }
+
+        public int brugerId
+        {
+            get { return _brugerId; }
+            set { _brugerId = value; }
+        }
+
+        public string fornavn
+        {
+            get { return _fornavn; }
+            set { _fornavn = value; }
+        }
+
+        public string efternavn
+        {
+            get { return _efternavn; }
+            set { _efternavn = value; }
+        }
+
+        public string email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public string by
+        {
+            get { return _by; }
+            set { _by = value; }
+        }
+
+        public int postnummer
+        {
+            get { return _postnummer; }
+            set { _postnummer = value; }
+        }
+
+        public string bilmodel
+        {
+            get { return _bilmodel; }
+            set { _bilmodel = value; }
+        }
+
+        public string bilfarve
+        {
+            get { return _bilfarve; }
+            set { _bilfarve = value; }
+        }
+
+        public void OpdatereProfil()
+            // Denne metode gør det muligt for brugeren at opdatere sin profil
+        {
+            // her skal metoden til at opdatere profilen stå
+        }
+    }
+}
---
+++ ZeaRide/Model/Samkørsel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeaRide.Model
+{
+    class Samkørsel
+    {
+        private int _samkørselId;
+        private string _fra;
+        private string _til;
+        private string _bemærkninger;
+        private DateTime _dato;
+        private TimeSpan _tid;
+        private bool _passager;
+
+        public Samkørsel(int SamkørselId, string Fra, string Til, string Bemærkninger, DateTime Dato, TimeSpan Tid, bool Passager)
+        {
+            _samkørselId = SamkørselId;
+            _fra = Fra;
+            _til = Til;
+            _bemærkninger = Bemærkninger;
+            _dato = Dato;
+            _tid = Tid;
+            _passager = Passager;
+        }
+
+        public int samkørselId
+        {
+            get { return _samkørselId; }
+            set { _samkørselId = value; }
+        }
+
+        public string fra
+        {
+            get { return _fra; }
+            set { _fra = value; }
+        }
+
+        public string til
+        {
+            get { return _til; }
+            set { _til = value; }
+        }
+
+        public string bemærkninger
+        {
+            get { return _bemærkninger; }
+            set { _bemærkninger = value; }
+        }
+
+        public DateTime dato
+        {
+            get { return _dato; }
+            set { _dato = value; }
+        }
+
+        public TimeSpan tid
+        {
+            get { return _tid; }
+            set { _tid = value; }
+        }
+
+        public bool passager
+        {
+            get { return _passager; }
+            set { _passager = value; }
+        }
+
+        public string OpretSamkørsel()
+            // Det her er metoden hvor brugeren kan oprette en samkørsel
+        {
+            // her skal koden stå
+        }
+
+        public List<Samkørsel> ListSamkørsel()
+            // Når en samkørsel bliver oprettet vil det blive overført til en liste med andre samkørsler
+        {
+            // her skal koden stå
+        }
+
+        public void SletSamkørsel()
+            // Hvis brugeren gerne vil have, at der skal slettes en samkørsel, så vil denne funktion hjælpe med det
+        {
+            // her skal koden stå
+        }
+    }
+}
