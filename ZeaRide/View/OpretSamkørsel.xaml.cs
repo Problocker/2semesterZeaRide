dea05edab569cdcaeb822c373a7c93eb6309151a
Nikolai Køhler Lundberg Hansen <niko9946@edu.easj.dk>
2020-05-19 14:35:56 +0200

    Nikolaj har lavet "opret samkørsel" GUI

--- ZeaRide/View/OpretSamkørsel.xaml.cs
+++ ZeaRide/View/OpretSamkørsel.xaml.cs
@@ -26,5 +26,10 @@ namespace ZeaRide.View
         {
             this.InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
