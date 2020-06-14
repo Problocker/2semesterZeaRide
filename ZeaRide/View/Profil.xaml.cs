e2a2f810296c394514b7b10ca620a8f4ce50ac44
Martin Jørgen Pedersen <mart124h@edu.easj.dk>
2020-06-14 12:25:59 +0200

    ffef

--- ZeaRide/View/Profil.xaml.cs
+++ ZeaRide/View/Profil.xaml.cs
@@ -26,5 +26,10 @@ namespace ZeaRide.View
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
